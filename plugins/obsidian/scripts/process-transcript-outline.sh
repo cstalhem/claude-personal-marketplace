164b2ea4769544a7fca91d0ab5ee92964f469da7
Carl Stålhem <carl@stalhem.se>
2026-01-23 15:58:27 +0100

    Skip numeric WebVTT cue IDs

--- plugins/obsidian/scripts/process-transcript-outline.sh
+++ plugins/obsidian/scripts/process-transcript-outline.sh
@@ -133,6 +133,9 @@ awk '
       if (line == "WEBVTT") {
         continue
       }
+      if (line ~ /^[0-9]+$/) {
+        continue
+      }
       if (line ~ /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}\/[0-9]+-[0-9]+$/) {
         continue
       }
